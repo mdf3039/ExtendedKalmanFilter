f22fb4ee25a172c91302904e9f9cfa038080aeef
mdf3039 <mdf3039@gmail.com>
2017-10-16 17:28:39 -0500

    Ninth commit - Put hashtag instead of // for comments

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -201,7 +201,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     h_of_x << sqrt(pow(ekf_.x_[0],2)+pow(ekf_.x_[1],2)),
                 atan(ekf_.x_[1]/ekf_.x_[0]),
                 (ekf_.x_[0]*ekf_.x_[2]+ekf_.x_[1]*ekf_.x_[3])/sqrt(pow(ekf_.x_[0],2)+pow(ekf_.x_[1],2));
-    #obtain the vector of raw measurements
+    //obtain the vector of raw measurements
     VectorXd z;
     z = VectorXd(3);
     z << measurement_pack.raw_measurements_[0],measurement_pack.raw_measurements_[1],
@@ -213,7 +213,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     while(z[1]>3.14159265){
         z[1]-=2*3.14159265;
     }
-    VectorXd y << z - h_of_x;
+    VectorXd y = z - h_of_x;
     //find K, using the H_jacobian
     Hj_ = CalculateJacobian(ekf_.x_);
 
