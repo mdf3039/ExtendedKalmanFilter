dcaa4a2ea2dc8f8aba4e9a22029fd40f6d9e3ff1
mdf3039 <mdf3039@gmail.com>
2017-10-20 20:19:06 -0500

    20th commit - Updated Hessian to not change with velocity.

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -14,11 +14,11 @@ MatrixXd CalculateJacobian(const VectorXd& x_state) {
     //obtain negations when needed
     float a = 1.0;
     if (x_state(0)<0){
-        a = -1.0;
+        a = 1.0;
     }
     float b = 1.0;
     if (x_state(1)<0){
-        b = -1.0;
+        b = 1.0;
     }
 	MatrixXd Hj(3,4);
 	//recover state parameters
