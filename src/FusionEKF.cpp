00e6c89a849e338692ae7f2b4a99f76b53916a5c
mdf3039 <mdf3039@gmail.com>
2017-10-22 01:08:46 -0500

    26th commit - made only one range rate

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -217,15 +217,15 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     float pi = 3.14159265;
     float angle_r = atan2(ekf_.x_[1]/ekf_.x_[0]);
     float range_rate = (ekf_.x_[0]*ekf_.x_[2]+ekf_.x_[1]*ekf_.x_[3])/sqrt(pow(ekf_.x_[0],2)+pow(ekf_.x_[1],2));
-    if ((ekf_.x_[0]<0)&&(ekf_.x_[1]>0)){
+    /*if ((ekf_.x_[0]<0)&&(ekf_.x_[1]>0)){
         //float angle_r = pi+atan(ekf_.x_[1]/ekf_.x_[0]);
-        range_rate = (ekf_.x_[0]*ekf_.x_[2]+ekf_.x_[1]*ekf_.x_[3])/sqrt(pow(ekf_.x_[0],2)+pow(ekf_.x_[1],2));
+        //range_rate = (ekf_.x_[0]*ekf_.x_[2]+ekf_.x_[1]*ekf_.x_[3])/sqrt(pow(ekf_.x_[0],2)+pow(ekf_.x_[1],2));
     }
     else if ((ekf_.x_[0]<0)&&(ekf_.x_[1]<0)){
         //float angle_r = -1.0*pi+atan(ekf_.x_[1]/ekf_.x_[0]);
-        range_rate = (ekf_.x_[0]*ekf_.x_[2]+ekf_.x_[1]*ekf_.x_[3])/sqrt(pow(ekf_.x_[0],2)+pow(ekf_.x_[1],2));
+        //range_rate = (ekf_.x_[0]*ekf_.x_[2]+ekf_.x_[1]*ekf_.x_[3])/sqrt(pow(ekf_.x_[0],2)+pow(ekf_.x_[1],2));
     }
-
+    */
     h_of_x << sqrt(pow(ekf_.x_[0],2)+pow(ekf_.x_[1],2)),
                 angle_r,
                 range_rate;
